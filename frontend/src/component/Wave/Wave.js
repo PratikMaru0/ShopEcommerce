2d9aabb4e5f1081e500bf65651bf3f7d3c7b9085
pratik <pratikmaru80762@gmail.com>
2022-01-02 12:34:56 +0530

    added wave

---
+++ frontend/src/component/Wave/Wave.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Wave.css"
+
+const Wave = () => {
+    return (
+        <div>
+            <section>
+                <div class="wave wave1"></div>
+                <div class="wave wave2"></div>
+                <div class="wave wave3"></div>
+                <div class="wave wave4"></div>
+            </section>
+        </div>
+    )
+}
+
+export default Wave
