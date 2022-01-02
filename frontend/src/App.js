82727c7df35bbe8fa6089dc4d86a46c32fa8d2fe
pratik <pratikmaru80762@gmail.com>
2022-01-02 13:10:45 +0530

    added wave

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
 import React from "react";
 import Footer from "./component/layout/Footer/Footer";
-import Wave from "./component/Wave/Wave";
 import Home from "./component/Home/Home";
 import ProductDetails from "./component/Product/ProductDetails";
 import Products from "./component/Product/Products";
@@ -188,7 +187,7 @@ function App() {
       </Switch>
 
       <Footer />
-      <Wave />
+
     </Router>
   );
 }
