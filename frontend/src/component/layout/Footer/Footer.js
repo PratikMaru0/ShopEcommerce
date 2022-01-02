5da28df3c1c6d7218026bbdc4b0cd8b07076f653
pratik <pratikmaru80762@gmail.com>
2022-01-02 19:09:17 +0530

    added logo

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -1,34 +1,33 @@
-import React from "react";
-import playStore from "../../../images/playstore.png";
-import appStore from "../../../images/Appstore.png";
-import "./Footer.css";
+// ReactArrowFunctionComponentES7
+import React from 'react'
+import playStore from "../../../images/playstore.png"
+import appStore from "../../../images/Appstore.png"
+import Logo from "../../../images/Logo1.jpg"
+import "./Footer.css"
 
 const Footer = () => {
-  return (
-    <footer id="footer">
-      <div className="leftFooter">
-        <h4>DOWNLOAD OUR APP</h4>
-        <p>Download App for Android and IOS mobile phone</p>
-        <img src={playStore} alt="playstore" />
-        <img src={appStore} alt="Appstore" />
-      </div>
+    return (
+        <footer id="footer">
+            <div className="leftFooter">
+                <h4><u>DOWNLOAD OUR APP</u></h4>
+                <p>Download App for Android and IOS</p>
+                <img src={playStore} alt="playstore" />
+                <img src={appStore} alt="appstore" />
+            </div>
+            <div className="midFooter">
+                <a href="/"><img src={Logo} alt="logo" class="Logo" /></a>
+                <p>Crafted Passionately</p>
+                <p>Copyrights 2022 &copy; MePratikMaru</p>
+            </div>
+            <div className="rightFooter">
+                <h4><u>Follow us on</u></h4>
+                <a href="https://www.instagram.com/patrick.__01/">Instagram</a>
+                {/* FIXME: here i didnt mentioned my facebook profile */}
+                <a href="https://www.instagram.com/patrick.__01/">Facebook</a>
+                <a href="https://twitter.com/pratikmaru01">Twitter</a>
+            </div>
+        </footer>
+    )
+}
 
-      <div className="midFooter">
-        {/* <h1>ECOMMERCE.</h1> */}
-        <img src="C:\Users\prati\OneDrive\Desktop\mernProjectEcommerce\frontend\src\images\Logo1.jpg" alt="" />
-        <p>High Quality is our first priority</p>
-
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
-      </div>
-
-      <div className="rightFooter">
-        <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+export default Footer
