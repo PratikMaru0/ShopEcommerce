8e4fafabb363fd887b24f9e21ed560c45ea08222
pratik <pratikmaru80762@gmail.com>
2022-01-03 20:48:14 +0530

    contact page

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -34,33 +34,33 @@ const Contact = () => {
           <div className="c-info">
             <div className="c-info-item">
               <img src={Phone} alt="" className="c-icon" />
-              +1 12 1234 45
+              +91 9112023344
             </div>
             <div className="c-info-item">
               <img src={Email} alt="" className="c-icon" />
-              pratikmaru80762@gmail.com
+              pratikmarutest@gmail.com
             </div>
             <div className="c-info-item">
               <img src={Address} alt="" className="c-icon" />
-              Sector-7 Shanti nagar Mira Road (East) 401107
+              Shanti Nagar Mira Road (East) Thane Maharashtra Pin code :- 401107
             </div>
           </div>
         </div>
         <div className="c-right">
           <p className="c-desc">
             <b>
-              Whats your story ?
+              What's your Feedback / Complaints ?
             </b>
-            <p> Get in touch. Always ready and available for freelancing if the right project comes along me.</p>
+            <p> Get in touch with us. Always ready and available for best service and user experience.</p>
           </p>
 
-          <form action="" ref={formRef} onSubmit={handleSubmit}>
+          <form className="contactForm" action="" ref={formRef} onSubmit={handleSubmit}>
             <input type="text" placeholder="Name" name="user_name" />
             <input type="text" placeholder="Subject" name="user_subject" />
             <input type="text" placeholder="Email" name="user_email" />
-            <textarea name="5" id="" cols="30" rows="10" placeholder="Message" name="message" className="msg"></textarea>
+            <textarea name="5" id="" cols="30" rows="6" placeholder="Message" name="message" className="msg"></textarea>
 
-            <button>Submit</button><br />
+            <button className="contactBtn">Submit</button><br />
             {done && "Thank you.... Got ur msg. I'll reply ASAP"}
 
           </form>
