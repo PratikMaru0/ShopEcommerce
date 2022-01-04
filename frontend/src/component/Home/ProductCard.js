71d6a9cd44f2360f43841921e2f02846ece9a901
pratik <pratikmaru80762@gmail.com>
2022-01-04 13:18:43 +0530

    added transition

--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -20,7 +20,7 @@ const ProductCard = ({ product }) => {
           ({product.numOfReviews} Reviews)
         </span>
       </div>
-      <span>{`₹ ${product.price}`}</span>
+      <span>{`₹ ${product.price} /- `}</span>
     </Link>
   );
 };
